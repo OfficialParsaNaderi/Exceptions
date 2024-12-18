025c753da4b5d03bd516c828659e9047112b4b18
OfficialParsaNaderi <parsanaderi1387.programmer@gmail.com>
2024-12-18 21:43:41 +0330

    PUSH .!

--- P66.py
+++
@@ -1,7 +0,0 @@
-file_1 = "text1.txt"
-file = open (file_1 , "r+")
-print(f"{file.readline()}\n")
-print(f"{file.readline()}\n")
-print(f"{file.readline()}\n")
-print(f"{file.readline()}\n")
-file.close()
